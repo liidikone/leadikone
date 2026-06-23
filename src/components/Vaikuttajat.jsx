c382a4f2d9fd14ade500dc7fcf26dd7d05754215
liidikone <sankariliidit@gmail.com>
2026-06-23 23:24:30 +0300

    Update Vaikuttajat.jsx

--- src/components/Vaikuttajat.jsx
+++ src/components/Vaikuttajat.jsx
@@ -176,7 +176,7 @@ export default function Vaikuttajat() {
       <div className="vaikuttajat__inner">
         <div className="vaikuttajat__header">
           <h2 className="vaikuttajat__title">
-            "SOMET<span className="vaikuttajat__title-on">ON</span>"
+            SOMET<span className="vaikuttajat__title-on">ON</span>
             <span className="vaikuttajat__title-accent">vaikuttajilla</span>
           </h2>
           <p className="vaikuttajat__lead">
